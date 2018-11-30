f78c81950071d1e645be39030d6dc19b4b4cfa1d
Naw Thue Llay Htoo <thue.llay@onepip.net>
2018-11-30 16:09:15 +0800

    rename item model

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Thue Llay on 29/11/18.
+//  Copyright © 2018 Thue Llay. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
